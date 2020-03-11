33bbeab090c78cd32300a080c3874775d9df1b8f
Victor Sanchez <v08311996@gmail.com>
2020-03-11 19:58:53 -0400

    Updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router,Route, Link } from "react-router-dom";
+
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
+
+const Navigation = () => {
+    return (
+      <div>
+        <div className="App">
+          <h1>React Router</h1>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/about">About</Link>
+          </div>
+          <div>
+          <Link to="/contact">Contact</Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  const App = () => (
+    <div>
+       <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Navigation />
+      </div>
+    </div>
+  );
+  
+  
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
